1836b75db1d30e3303a46dc121d5e4b72da09d53
BernardoGiordano <bernardogiordano369@gmail.com>
2016-05-15 10:33:12 +0200

    Source code for v1.0

---
+++ source/main.c
@@ -0,0 +1,132 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <3ds.h>
+#include "http.h"
+#include "PID.h"
+
+void intro(PrintConsole topScreen, PrintConsole bottomScreen){
+	consoleSelect(&topScreen);
+	printf("\x1b[2J");
+	printf("\x1b[47;30m     Pokemon Event Catchers Italia Tool v1.0      \x1b[0m");
+	printf("\nA - PID Checker");
+	printf("\nX - Worldwide distributions");
+	printf("\nY - Our distributions");
+	printf("\x1b[29;15HPress Start to exit.");	
+	consoleSelect(&bottomScreen);
+	printf("\nECI Tool is the official Homebrew of theFB community '\x1b[32mPokemon Event Catchers\nItalia\x1b[0m'.\n\nThis is meant to be a general purpose   application who could serve both event\ncollectors and classic players of the\ngame.\n\nYou can join us at:\n\x1b[32mfacebook.com/groups/PokemonEventCatchersItalia\x1b[0m");
+	printf("\x1b[26;0HECI Tool");
+	printf("\n\nDeveloped by Bernardo Giordano.");
+}
+
+int main() {	
+	gfxInitDefault();
+	PrintConsole topScreen, bottomScreen;
+	consoleInit(GFX_TOP, &topScreen);
+	consoleInit(GFX_BOTTOM, &bottomScreen);
+	
+	consoleSelect(&topScreen);
+	intro(topScreen, bottomScreen);
+	
+	while (aptMainLoop()) {
+		gspWaitForVBlank();
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+		
+		if (kDown & KEY_START) 
+			break; 
+		
+		if (kDown & KEY_A) {
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			printf("----------------------------------------");
+			printf("\n\x1A\x1B - Move cursor\n");
+			printf("\x18\x19 - Change value (0-9/A-F)\n");
+			printf("SELECT - Reset values\n\n");
+			printf("----------------------------------------");
+			printf("\x1b[19;0H----------------------------------------");
+			printf("\x1b[20;0HSometimes Highest IV test \x1b[31mdoesn't work\x1b[0m. This isn't related to the implementationof this software, but with the formulas behind this.\nFormulas can be found at:\n\x1b[32mbulbapedia.bulbagarden.net/wiki/Personality_value\x1b[0m");
+			printf("\x1b[27;0H----------------------------------------");
+			printf("\x1b[29;10HPress START to exit.");
+			consoleSelect(&topScreen);
+			PID();
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			consoleSelect(&topScreen);			
+			intro(topScreen, bottomScreen);
+		}
+		
+		if (kDown & KEY_X) {
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			printf("----------------------------------------");
+			printf("NA  - North America");
+			printf("\nPAL - Europe, Australia");
+			printf("\nJPN - Japan");
+			printf("\nKOR - South Korea");
+			printf("\nALL - All regions available\n");
+			printf("----------------------------------------");
+			printf("\x1b[27;0H    Please check your connection....");
+			printf("\x1b[29;10HPress A to continue.");
+			consoleSelect(&topScreen);		
+			printf("\x1b[2J");
+			getText("http://eventcatchersitalia.altervista.org/10/worldwide1.txt");
+			
+			while (aptMainLoop()) {
+				gspWaitForVBlank();
+				hidScanInput();
+
+				u32 kDown = hidKeysDown();
+				if (kDown & KEY_A) 
+					break; 			 
+			}
+			
+			printf("\x1b[2J");
+			getText("http://eventcatchersitalia.altervista.org/10/worldwide2.txt");
+			
+			while (aptMainLoop()) {
+				gspWaitForVBlank();
+				hidScanInput();
+
+				u32 kDown = hidKeysDown();
+				if (kDown & KEY_A) 
+					break; 			 
+			}
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			consoleSelect(&topScreen);
+			intro(topScreen, bottomScreen);
+		}
+		
+		if (kDown & KEY_Y) {
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			printf("\x1b[27;0H    Please check your connection....");
+			printf("\x1b[29;10HPress START to exit.");
+			consoleSelect(&topScreen);		
+			printf("\x1b[2J");
+			getText("http://eventcatchersitalia.altervista.org/10/giveaway.txt");
+			
+			while (aptMainLoop()) {
+				gspWaitForVBlank();
+				hidScanInput();
+
+				u32 kDown = hidKeysDown();
+				if (kDown & KEY_START) 
+					break; 			 
+			}
+			
+			consoleSelect(&bottomScreen);
+			printf("\x1b[2J");
+			consoleSelect(&topScreen);
+			intro(topScreen, bottomScreen);
+		}
+					 
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+	}
+
+	gfxExit();
+	return 0;
+}
+
---
+++ source/pid.c
@@ -0,0 +1,397 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <3ds.h>
+
+#define DELAY 25
+
+void hexa2bin (int binary[], char hexadecimal[], int nbit) {
+	int k = 0;
+	int i = 0;
+	
+	for (i = 0; i < nbit; i++) {
+		k = i * 4;
+		switch (hexadecimal[i]) {
+			case '0' : {
+				binary[k] = 0;
+				binary[k+1] = 0;
+				binary[k+2] = 0;
+				binary[k+3] = 0;
+				break;
+			}
+			case '1' : {
+				binary[k] = 0;
+				binary[k+1] = 0;
+				binary[k+2] = 0;
+				binary[k+3] = 1;
+				break;
+			}
+			case '2' : {
+				binary[k] = 0;
+				binary[k+1] = 0;
+				binary[k+2] = 1;
+				binary[k+3] = 0;
+				break;
+			}
+			case '3' : {
+				binary[k] = 0;
+				binary[k+1] = 0;
+				binary[k+2] = 1;
+				binary[k+3] = 1;
+				break;
+			}
+			case '4' : {
+				binary[k]=0;
+				binary[k+1]=1;
+				binary[k+2]=0;
+				binary[k+3]=0;
+				break;
+			}
+			case '5' : {
+				binary[k]=0;
+				binary[k+1]=1;
+				binary[k+2]=0;
+				binary[k+3]=1;
+				break;
+			}
+			case '6' : {
+				binary[k]=0;
+				binary[k+1]=1;
+				binary[k+2]=1;
+				binary[k+3]=0;
+				break;
+			}
+			case '7' : {
+				binary[k]=0;
+				binary[k+1]=1;
+				binary[k+2]=1;
+				binary[k+3]=1;
+				break;
+			}
+			case '8' : {
+				binary[k]=1;
+				binary[k+1]=0;
+				binary[k+2]=0;
+				binary[k+3]=0;
+				break;
+			}
+			case '9' : {
+				binary[k]=1;
+				binary[k+1]=0;
+				binary[k+2]=0;
+				binary[k+3]=1;
+				break;
+			}	
+			case 'A' : {
+				binary[k]=1;
+				binary[k+1]=0;
+				binary[k+2]=1;
+				binary[k+3]=0;
+				break;
+			}	
+			case 'B' : {
+				binary[k]=1;
+				binary[k+1]=0;
+				binary[k+2]=1;
+				binary[k+3]=1;
+				break;
+			}
+			case 'C' : {
+				binary[k]=1;
+				binary[k+1]=1;
+				binary[k+2]=0;
+				binary[k+3]=0;
+				break;
+			}
+			case 'D' : {
+				binary[k]=1;
+				binary[k+1]=1;
+				binary[k+2]=0;
+				binary[k+3]=1;
+				break;
+			}	
+			case 'E' : {
+				binary[k]=1;
+				binary[k+1]=1;
+				binary[k+2]=1;
+				binary[k+3]=0;
+				break;
+			}
+			case 'F' : {
+				binary[k]=1;
+				binary[k+1]=1;
+				binary[k+2]=1;
+				binary[k+3]=1;
+				break;
+			}						
+		}
+	}
+}
+
+long unsigned int hexa2dec (char hexadecimal[], int binary[], int nbit) {
+	int i = 0;
+	long unsigned int decimal = 0;
+	
+	hexa2bin (binary, hexadecimal, nbit);
+	
+	for (i = 0; i < nbit * 4; i++) {
+		long unsigned int pot = 1;
+		int j = 0;
+		while (j < i) {
+			pot *= 2;
+			j++;
+		}
+		decimal += binary[nbit * 4 - 1 - i] * pot;
+	}
+	return decimal;
+}
+
+void PIDchecker(char pid_hexa_vett[], int var[]) {
+	int pid_bin_vett[8 * 4];
+	int tid_vett[16];
+	int sid_vett[16];
+	int xor_vett[16];
+	
+	int i = 0;
+	long unsigned int decimal = 0;
+	long unsigned int xor_result = 0;
+	int tid_dec = var[0] * 10000 + var[1] * 1000 + var[2] * 100 + var[3] * 10 + var[4] * 1; 
+	int sid_dec = var[5] * 10000 + var[6] * 1000 + var[7] * 100 + var[8] * 10 + var[9] * 1;
+	
+	decimal = hexa2dec (pid_hexa_vett, pid_bin_vett, 8);
+	
+	printf("\x1b[5;0H-Nature (III/IV gen only): ");
+	switch (decimal % 25) {	
+		case 0 : { printf("Hardy   "); break;}
+		case 1 : { printf("Lonely  "); break;}	
+		case 2 : { printf("Brave   "); break;}
+		case 3 : { printf("Adamant "); break;}	
+		case 4 : { printf("Naughty "); break;}	
+		case 5 : { printf("Bold    "); break;}
+		case 6 : { printf("Docile  "); break;}
+		case 7 : { printf("Relaxed "); break;}
+		case 8 : { printf("Impish  "); break;}	
+		case 9 : { printf("Lax     "); break;}		
+		case 10 : { printf("Timid   "); break;}
+		case 11 : { printf("Hasty   "); break;}
+		case 12 : { printf("Serious "); break;}				
+		case 13 : { printf("Jolly   "); break;}
+		case 14 : { printf("Naive   "); break;}	
+		case 15 : { printf("Modest  "); break;}	
+		case 16 : { printf("Mild    "); break;}
+		case 17 : { printf("Quiet   "); break;}
+		case 18 : { printf("Bashful "); break;}
+		case 19 : { printf("Rash    "); break;}	
+		case 20 : { printf("Calm    "); break;}		
+		case 21 : { printf("Gentle  "); break;}	
+		case 22 : { printf("Sassy   "); break;}
+		case 23 : { printf("Careful "); break;}	
+		case 24 : { printf("Quirky  "); break;}		
+	}
+	
+	printf("\x1b[6;0H-Highest IV(it can fail): ");
+	switch (decimal % 6) {
+		case 0 : { printf("HP     "); break;	}
+		case 1 : { printf("Attack "); break;	}	
+		case 2 : { printf("Defense"); break;	}	
+		case 4 : { printf("Sp. Atk"); break;	}			
+		case 5 : { printf("Sp. Def"); break;	}			
+		case 3 : { printf("Speed  "); break;	}					
+	}
+	
+	//SHINYNESS
+	for (i = 0; i < 16; i++) {
+		if ((tid_dec % 2) == 0) 
+			tid_vett[15 - i] = 0;
+		else 
+			tid_vett[15 - i] = 1;
+		if ((sid_dec % 2) == 0) 
+			sid_vett[15 - i] = 0;
+		else 
+			sid_vett[15 - i] = 1;
+		tid_dec /= 2;
+		sid_dec /= 2;			
+	}
+		
+	for (i = 0; i < 16; i++) {
+		if (pid_bin_vett[i] == pid_bin_vett[16 + i])
+			xor_vett[i] = 0;
+		else 
+			xor_vett[i] = 1;
+		
+		if (xor_vett[i] == tid_vett[i])
+			xor_vett[i] = 0;
+		else
+			xor_vett[i] = 1;
+		
+		if (xor_vett[i] == sid_vett[i])
+			xor_vett[i] = 0;
+		else
+			xor_vett[i] = 1;
+	}
+
+	for (i = 0; i < 16; i++) {
+		long unsigned int pot = 1;
+		int j = 0;
+		while (j < i) {
+			pot *= 2;
+			j++;
+		}
+		xor_result += xor_vett[15 - i] * pot;	
+	}
+	
+	printf ("\x1b[7;0H-Shiny in III-V gen: ");
+	if (xor_result < 8)
+		printf("\x1b[32mTRUE \x1b[0m");
+	else 
+		printf("\x1b[31mFALSE\x1b[0m");
+	
+	printf ("\x1b[8;0H-Shiny in VI gen: ");
+	if (xor_result < 16)
+		printf("\x1b[32mTRUE \x1b[0m");
+	else
+		printf("\x1b[31mFALSE\x1b[0m");
+}
+
+void posCursore (char cursore[], int pos[], int temp) {
+	cursore[pos[0]] = ' ';
+	cursore[pos[0] + temp] = '^';
+	pos[0] += temp;
+}
+
+void incrementa(int var[], int posizione[], int temp) {
+	var[posizione[0]] += temp;
+}
+
+void incrementaPID(char pid[], int counterPID[], char varPID[], int posizione[], int temp) {
+	counterPID[posizione[0] - 10] += temp;
+	pid[posizione[0] - 10] = varPID[counterPID[posizione[0] - 10]];
+}
+
+void checkValDec(int var[]) {
+	int tempTid = var[0] * 10000 + var[1] * 1000 + var[2] * 100 + var[3] * 10 + var[4] * 1; 
+	int tempSid = var[5] * 10000 + var[6] * 1000 + var[7] * 100 + var[8] * 10 + var[9] * 1;
+	if (tempTid >= 65536) {
+		var[0] = 6;
+		var[1] = 5;
+		var[2] = 5;
+		var[3] = 3;
+		var[4] = 5;
+	}
+	if (tempSid >= 65536) {
+		var[5] = 6;
+		var[6] = 5;
+		var[7] = 5;
+		var[8] = 3;
+		var[9] = 5;
+	}
+}
+
+void printCursore(char cur[]) {
+	printf("\x1b[3;0H----%c%c%c%c%c",cur[0], cur[1], cur[2], cur[3], cur[4]);
+	printf("--------------%c%c%c%c%c",cur[5], cur[6], cur[7], cur[8], cur[9]);
+	printf("--------------%c%c%c%c%c%c%c%c",cur[10], cur[11], cur[12], cur[13], cur[14], cur[15], cur[16], cur[17]);
+}
+
+void show(int var[], char pid[]) {
+	printf("\x1b[2;0HTID:%d%d%d%d%d",var[0], var[1], var[2], var[3], var[4]);
+	printf("          SID:%d%d%d%d%d",var[5], var[6], var[7], var[8], var[9]);
+	printf("          PID:%c%c%c%c%c%c%c%c", pid[0], pid[1], pid[2], pid[3], pid[4], pid[5], pid[6], pid[7]);
+}
+
+void PID(){
+	int posizione[1] = {0};
+	int var[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	char pid[8] = {'0', '0', '0', '0', '0', '0', '0', '0'};
+	char cursore[18] = {'^', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', };
+	char varPID[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+	int counterPID[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+	printf("\x1b[2J");
+	printf("\x1b[47;30m                   PID Checker                    \x1b[0m");
+	printf("---------------------------------------------------------");	
+	
+	show(var, pid);
+	printCursore(cursore);
+	
+	int t_frame = 0;
+	while (aptMainLoop()) {
+		gspWaitForVBlank();
+		hidScanInput();
+
+		u32 kDown = hidKeysDown();
+		
+		if ((kDown & KEY_DRIGHT || (hidKeysHeld() & KEY_DRIGHT && t_frame % DELAY == 0)) && (posizione[0] < 17)) {
+			posCursore(cursore, posizione, 1);
+			printCursore(cursore);
+		}
+		
+		if ((kDown & KEY_DLEFT || (hidKeysHeld() & KEY_DLEFT && t_frame % DELAY == 0)) && (posizione[0] > 0)) {
+			posCursore(cursore, posizione, -1);
+			printCursore(cursore);
+		}
+		
+		if ((kDown & KEY_DUP || (hidKeysHeld() & KEY_DUP && t_frame % DELAY == 0)) && (var[posizione[0]] < 6) && (posizione[0] == 0)) { // posizione 0
+			incrementa(var, posizione, 1);
+			checkValDec(var);
+			show(var, pid);
+			PIDchecker(pid, var);
+		}
+		if ((kDown & KEY_DUP || (hidKeysHeld() & KEY_DUP && t_frame % DELAY == 0)) && (var[posizione[0]] < 6) && (posizione[0] == 5)) { // posizione 5
+			incrementa(var, posizione, 1);
+			checkValDec(var);
+			show(var, pid);
+			PIDchecker(pid, var);
+		}
+		if ((kDown & KEY_DUP || (hidKeysHeld() & KEY_DUP && t_frame % DELAY == 0)) && (var[posizione[0]] < 9) && (posizione[0] > 0) && (posizione[0] < 10) && (posizione[0] != 5)) { // posizione 1,2,3,4,6,7,8,9
+			incrementa(var, posizione, 1);	
+			checkValDec(var);
+			show(var, pid);	
+			PIDchecker(pid, var);
+		}
+		if ((kDown & KEY_DUP || (hidKeysHeld() & KEY_DUP && t_frame % DELAY == 0)) && (posizione[0] > 9) && (counterPID[posizione[0] - 10] < 15)) {
+			incrementaPID(pid, counterPID, varPID, posizione, 1);
+			show(var, pid);
+			PIDchecker(pid, var);
+		}
+				
+		if ((kDown & KEY_DDOWN || (hidKeysHeld() & KEY_DDOWN && t_frame % DELAY == 0)) && (var[posizione[0]] > 0) && (posizione[0] >= 0) && (posizione[0] < 10)) { // tutte le posizioni
+			incrementa(var, posizione, -1);
+			show(var, pid);
+			PIDchecker(pid, var);
+		}
+		if ((kDown & KEY_DDOWN || (hidKeysHeld() & KEY_DDOWN && t_frame % DELAY == 0)) && (posizione[0] > 9) && (counterPID[posizione[0] - 10] > 0)) {
+			incrementaPID(pid, counterPID, varPID, posizione, -1);
+			show(var, pid);
+			PIDchecker(pid, var);
+		}
+		
+		if (kDown & KEY_SELECT) {
+			int j = 0;
+			posizione[0] = 0;
+			for (j = 0; j < 10; j++)
+				var[j] = 0;
+			for (j = 0; j < 8; j++)
+				pid[j] = '0'; 
+			cursore[0] = '^';
+			for (j = 1; j < 18; j++)
+			cursore[j] = ' ';
+			for (j = 1; j < 8; j++)
+			counterPID[j] = 0;
+			printf("\x1b[2J");
+			printf("\x1b[47;30m                   PID Checker                    \x1b[0m");
+			printf("---------------------------------------------------------");	
+			show(var, pid);
+			printCursore(cursore);						
+		}
+		
+		if (kDown & KEY_START)
+			break;
+		
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+		
+		if (t_frame > 90000) t_frame = 1;
+		t_frame++;
+		
+	}
+}
