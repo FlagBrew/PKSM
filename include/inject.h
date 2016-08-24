9e329ada3063f47bce69dd23173a80b22b6a6618
BernardoGiordano <bernardogiordano369@gmail.com>
2016-08-24 14:30:37 +0200

    now HMs are injected as well

--- include/inject.h
+++ include/inject.h
@@ -7,6 +7,6 @@ void injectMoney(u8* mainbuf, u64 i);
 void injectItem(u8* mainbuf, int i, u32 values[], int type, int nInjected[], int game);
 void injectBP(u8* mainbuf, int i, int game);
 void injectBadges(u8* mainbuf, int i);
-void injectTM(u8* mainbuf);
+void injectTM(u8* mainbuf, int game);
 void refreshValues(PrintConsole topScreen, int injectCont[], int nInjected[]);
 int saveFileEditor(PrintConsole topScreen, PrintConsole bottomScreen, u8* mainbuf, int game, int nInjected[], int injectCont[]);
