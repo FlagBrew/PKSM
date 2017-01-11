918685b2431e796fc9b89dcf70df3378035cb89b
Slownic <rixus90@gmail.com>
2017-01-11 21:55:57 +0100

    Update MainWindow.java

--- PKSM/servepkx/java/src/clientPKSM/MainWindow.java
+++ PKSM/servepkx/java/src/clientPKSM/MainWindow.java
@@ -16,10 +16,8 @@ public class MainWindow {
 
 	private JFrame frmPksmClient;
 	private JTextField txtHost;
-	private JTextField txtPort;
 	private File[] files;
 	private String host;
-	private int port;
 
 	/**
 	 * Launch the application.
@@ -50,30 +48,21 @@ public class MainWindow {
 	private void initialize() {
 		frmPksmClient = new JFrame();
 		frmPksmClient.setTitle("servepkx");
-		frmPksmClient.setBounds(100, 100, 562, 401);
+		frmPksmClient.setBounds(100, 100, 486, 401);
 		frmPksmClient.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frmPksmClient.getContentPane().setLayout(null);
 
 		txtHost = new JTextField();
-		txtHost.setBounds(131, 29, 123, 20);
+		txtHost.setBounds(150, 29, 191, 20);
 		frmPksmClient.getContentPane().add(txtHost);
 		txtHost.setColumns(10);
 
-		txtPort = new JTextField();
-		txtPort.setBounds(369, 29, 123, 20);
-		frmPksmClient.getContentPane().add(txtPort);
-		txtPort.setColumns(10);
-
 		JLabel lblIpAddress = new JLabel("IP Address:");
-		lblIpAddress.setBounds(37, 32, 84, 14);
+		lblIpAddress.setBounds(39, 32, 84, 14);
 		frmPksmClient.getContentPane().add(lblIpAddress);
 
-		JLabel lblPort = new JLabel("Port:");
-		lblPort.setBounds(305, 32, 57, 14);
-		frmPksmClient.getContentPane().add(lblPort);
-
-		JButton btnConfirm = new JButton("Confirm Host and Port");
-		btnConfirm.setBounds(184, 90, 191, 52);
+		JButton btnConfirm = new JButton("Confirm IP Address");
+		btnConfirm.setBounds(150, 80, 191, 52);
 		frmPksmClient.getContentPane().add(btnConfirm);
 		btnConfirm.addActionListener(new ActionListener() {
 
@@ -85,20 +74,15 @@ public class MainWindow {
 							JOptionPane.ERROR_MESSAGE);
 				} else {
 					host = txtHost.getText();
-					if (txtPort.getText().isEmpty()) {
-						port = 9000;
-					} else {
-						port = Integer.parseInt(txtPort.getText());
-					}
 					JOptionPane.showMessageDialog(frmPksmClient,
-							"The following host and port are currently set\nHost: " + host + "\nPort: " + port);
+							"The following IP Address is currently set\nHost: " + host);
 				}
 			}
 
 		});
 
 		JButton btnSelectFiles = new JButton("Select .pk6/.pk7 files");
-		btnSelectFiles.setBounds(184, 170, 191, 52);
+		btnSelectFiles.setBounds(150, 159, 191, 52);
 		frmPksmClient.getContentPane().add(btnSelectFiles);
 		btnSelectFiles.addActionListener(new ActionListener() {
 
@@ -117,18 +101,18 @@ public class MainWindow {
 		});
 
 		JButton btnSendFiles = new JButton("Send .pk6/.pk7 files");
-		btnSendFiles.setBounds(184, 250, 191, 52);
+		btnSendFiles.setBounds(150, 240, 191, 52);
 		frmPksmClient.getContentPane().add(btnSendFiles);
 		
-		JLabel lblPksmClientV = new JLabel("servepkx v1.0 - Made by Slownic. To use with PKSM, by Bernardo Giordano. 2017");
-		lblPksmClientV.setBounds(39, 338, 497, 14);
+		JLabel lblPksmClientV = new JLabel("servepkx v1.0 - Made by Slownic. To use with PKSM, by Bernardo Giordano.");
+		lblPksmClientV.setBounds(20, 338, 450, 14);
 		frmPksmClient.getContentPane().add(lblPksmClientV);
 		btnSendFiles.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				Client client = new Client(host, port);
+				Client client = new Client(host);
 				String result = "The following files have been sent:\n";
 
 				for (int i = 0; i < files.length; i++) {
