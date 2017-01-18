49c987915443d5604264abef37eabe72c96e3c72
Favna <jeroen.claassens@live.nl>
2017-01-18 15:49:39 +0100

    fix derp

--- Release.bat
+++ Release.bat
@@ -1,10 +1,10 @@
 make
 rd Release /s /q
 mkdir Release\3ds\PKSM Release\3ds\PKSV Release\servepx\java Release\servepx\python
-move output\3ds-arm\PKSM.cia Release
-move output\3ds-arm\3ds\PKSM\PKSM.3dsx Release\3ds\PKSM
-move ..\PKSV\output\3ds-arm\PKSV.cia Release
-move ..\PKSV\output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
+copy output\3ds-arm\PKSM.cia Release
+copy output\3ds-arm\3ds\PKSM\PKSM.3dsx Release\3ds\PKSM
+copy ..\PKSV\output\3ds-arm\PKSV.cia Release
+copy ..\PKSV\output\3ds-arm\3ds\PKSV\PKSV.3dsx Release\3ds\PKSV
 copy servepkx\java\servepkx.jar Release\servepx\java
 copy servepkx\java\servepkx.exe Release\servepx\java
 copy servepkx\java\README.md Release\servepx\java
@@ -13,3 +13,4 @@ copy servepkx\python\README.md Release\servepx\python
 cd Release
 zip -r PKSM.zip .
 cd ..\
+pause
