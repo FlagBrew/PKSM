cef92ab81c28e811fc71f41995d91d54a6b7d5f5
piepie62 <piepie62@users.noreply.github.com>
2018-05-21 12:15:39 -0400

    That never happened

--- source/i18n/LanguageStrings.cpp
+++ source/i18n/LanguageStrings.cpp
@@ -97,7 +97,6 @@ void LanguageStrings::loadGui(Language lang)
     static const std::string base = "romfs:/i18n/";
     std::string path = io::exists(base + folder(lang) + "gui.json") ? base + folder(lang) + "gui.json" : base + folder(Language::EN) + "gui.json";
 
-    std::string tmpKey, tmpVal;
     std::ifstream values(path);
     gui << values;
     values.close();
