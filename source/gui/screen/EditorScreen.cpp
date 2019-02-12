0d305916f4f4797a8c27c7eabe2c67d7914ccc11
piepie62 <christopherfeger@yahoo.com>
2019-02-11 18:10:54 -0700

    Use current time when default time set to 0

--- source/gui/screen/EditorScreen.cpp
+++ source/gui/screen/EditorScreen.cpp
@@ -113,9 +113,10 @@ EditorScreen::EditorScreen(std::shared_ptr<PKX> pokemon, int box, int index)
         pkm->fixMoves();
         // pkm->PID((u32)randomNumbers());
         pkm->language(Configuration::getInstance().language());
-        pkm->metDay(Configuration::getInstance().day());
-        pkm->metMonth(Configuration::getInstance().month());
-        pkm->metYear(Configuration::getInstance().year() - 2000);
+        const time_t current = time(NULL);
+        pkm->metDay(Configuration::getInstance().day() ? Configuration::getInstance().day() : gmtime(&current)->tm_mday);
+        pkm->metMonth(Configuration::getInstance().month() ? Configuration::getInstance().month() : gmtime(&current)->tm_mon);
+        pkm->metYear(Configuration::getInstance().year() ? Configuration::getInstance().year() - 2000 : gmtime(&current)->tm_year);
         pkm->metLevel(1);
         if (pkm->generation() == Generation::SIX)
         {
