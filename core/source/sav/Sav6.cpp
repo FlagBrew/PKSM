a6157418c16629d6a8d7595707dd2b6f9f8e39cc
piepie62 <christopherfeger@yahoo.com>
2019-08-12 16:54:39 -0400

    Fix Eon Ticket because GF likes special cases

--- core/source/sav/Sav6.cpp
+++ core/source/sav/Sav6.cpp
@@ -616,6 +616,12 @@ void Sav6::mysteryGift(WCX& wc, int& pos)
     WC6* wc6 = (WC6*)&wc;
     *(u8*)(data + WondercardFlags + wc6->ID() / 8) |= 0x1 << (wc6->ID() % 8);
     std::copy(wc6->rawData(), wc6->rawData() + 264, data + WondercardData + 264 * pos);
+    if (game == Game::ORAS && wc6->ID() == 2048 && wc6->object() == 726)
+    {
+        static constexpr u32 EON_MAGIC = 0x225D73C2;
+        *(u32*)(data + 0x319B8) = EON_MAGIC;
+        *(u32*)(data + 0x319DE) = EON_MAGIC;
+    }
     pos = (pos + 1) % 24;
 }
 
