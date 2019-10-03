3ecec4eaf36c231fc4d750db7c22d91672079922
piepie62 <christopherfeger@yahoo.com>
2019-10-02 21:03:50 -0700

    Change default button text WidthAction

--- common/source/gui/Button.cpp
+++ common/source/gui/Button.cpp
@@ -37,6 +37,6 @@ void Button::draw() const
     Gui::sprite(key, xPos, yPos);
     if (!text.empty())
     {
-        Gui::text(text, xPos + width / 2, yPos + height / 2, size, textColor, TextPosX::CENTER, TextPosY::CENTER, TextWidthAction::WRAP, width);
+        Gui::text(text, xPos + width / 2, yPos + height / 2, size, textColor, TextPosX::CENTER, TextPosY::CENTER, TextWidthAction::SQUISH_OR_SCROLL, width - 10);
     }
 }
