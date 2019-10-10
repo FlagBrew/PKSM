f65c519237588ce10240ce34d7f1197bbc81772b
piepie62 <christopherfeger@yahoo.com>
2019-10-09 23:04:06 -0700

    Don't add geolocation data on generation

--- 3ds/source/gui/screen/EditorScreen.cpp
+++ 3ds/source/gui/screen/EditorScreen.cpp
@@ -119,16 +119,12 @@ EditorScreen::EditorScreen(std::shared_ptr<PKX> pokemon, int box, int index, boo
         if (pkm->generation() == Generation::SIX)
         {
             ((PK6*)pkm.get())->consoleRegion(Configuration::getInstance().nationality());
-            ((PK6*)pkm.get())->geoCountry(0, Configuration::getInstance().defaultCountry());
-            ((PK6*)pkm.get())->geoRegion(0, Configuration::getInstance().defaultRegion());
             ((PK6*)pkm.get())->country(Configuration::getInstance().defaultCountry());
             ((PK6*)pkm.get())->region(Configuration::getInstance().defaultRegion());
         }
         else if (pkm->generation() == Generation::SEVEN)
         {
             ((PK7*)pkm.get())->consoleRegion(Configuration::getInstance().nationality());
-            ((PK7*)pkm.get())->geoCountry(0, Configuration::getInstance().defaultCountry());
-            ((PK7*)pkm.get())->geoRegion(0, Configuration::getInstance().defaultRegion());
             ((PK7*)pkm.get())->country(Configuration::getInstance().defaultCountry());
             ((PK7*)pkm.get())->region(Configuration::getInstance().defaultRegion());
         }
