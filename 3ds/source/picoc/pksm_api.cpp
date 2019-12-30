971d77417c8da94e1d95b2bb77c77a5d7756e3ae
piepie62 <christopherfeger@yahoo.com>
2019-12-29 23:37:34 -0700

    Add profanity filter to gui_keyboard

--- 3ds/source/picoc/pksm_api.cpp
+++ 3ds/source/picoc/pksm_api.cpp
@@ -185,7 +185,7 @@ void gui_keyboard(struct ParseState* Parser, struct Value* ReturnValue, struct V
     SwkbdState state;
     swkbdInit(&state, SWKBD_TYPE_NORMAL, 1, numChars);
     swkbdSetHintText(&state, hint);
-    swkbdSetValidation(&state, SWKBD_NOTBLANK_NOTEMPTY, 0, 0);
+    swkbdSetValidation(&state, SWKBD_NOTBLANK_NOTEMPTY, SWKBD_FILTER_PROFANITY, 0);
     swkbdInputText(&state, out, numChars * 3); // numChars is UTF-16 codepoints, each UTF-8 codepoint needs up to 3 bytes, so
     out[numChars * 3 - 1] = '\0';
 }
