0d7263383772d84d39e1688c1d9017ba42e8102a
piepie62 <christopherfeger@yahoo.com>
2020-05-10 15:36:13 -0400

    Remove testing code

--- 3ds/source/gui/gui.cpp
+++ 3ds/source/gui/gui.cpp
@@ -826,10 +826,6 @@ void Gui::mainLoop(void)
 {
     bool exit = false;
     Sound::start();
-    C2D_TextBuf c2dText = C2D_TextBufNew(256);
-    C2D_Text displayMe;
-    C2D_TextParse(&displayMe, c2dText, "Wonder what happens with this?\n\nProbably breaks");
-    C2D_TextOptimize(&displayMe);
     while (aptMainLoop() && !exit)
     {
         hidScanInput();
@@ -857,8 +853,6 @@ void Gui::mainLoop(void)
             }
             drawNoHome();
 
-            C2D_DrawText(&displayMe, C2D_AlignJustified, 160, 20, 1.0f, FONT_SIZE_12, FONT_SIZE_12);
-
             C3D_FrameEnd(0);
             Gui::frameClean();
             inFrame = false;
@@ -871,7 +865,6 @@ void Gui::mainLoop(void)
 
             target(GFX_BOTTOM);
             screens.top()->doBottomDraw();
-            Gui::text(std::to_string(displayMe.words), 20, 20, 1.0f, COLOR_BLACK, TextPosX::LEFT, TextPosY::CENTER);
             flushText();
 
             if (!aptIsHomeAllowed() && aptIsHomePressed())
@@ -879,8 +872,6 @@ void Gui::mainLoop(void)
                 setDoHomeDraw();
             }
             drawNoHome();
-
-            // C2D_DrawText(&displayMe, C2D_AlignJustified, 20, 20, 1.0f, 1.0f, 1.0f);
 
             C3D_FrameEnd(0);
             Gui::frameClean();
