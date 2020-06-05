25e55bd9c8bd3e0033c7946cc73c0e6f13ffa0df
piepie62 <christopherfeger@yahoo.com>
2020-06-05 19:01:08 -0400

    Add sav_set_<type> and JSON script APIs Remove sav_gbo and sav_sbo in preparation for parameter changes

--- common/source/picoc/library_pksm.c
+++ common/source/picoc/library_pksm.c
@@ -17,8 +17,6 @@ struct LibraryFunction UnixFunctions[] =
     { gui_numpad,           "void gui_numpad(unsigned int* out, char* hint, int maxDigits);" },
     { gui_boxes,            "int gui_boxes(int* fromStorage, int* box, int* slot, int doCrypt);" },
     // sav and storage
-    { sav_sbo,              "int sav_sbo();" },
-    { sav_gbo,              "int sav_gbo();" },
     { sav_boxDecrypt,       "void sav_box_decrypt();" },
     { sav_boxEncrypt,       "void sav_box_encrypt();" },
     { sav_get_pkx,          "void sav_get_pkx(char* data, int box, int slot);" },
@@ -27,8 +25,6 @@ struct LibraryFunction UnixFunctions[] =
     { sav_wcx_free_slot,    "int sav_wcx_free_slot();" },
     { sav_get_value,        "int sav_get_value(enum SAV_Field field, ...);" },
     { sav_get_max,          "int sav_get_max(enum SAV_MaxField field, ...);" },
-    { sav_get_string,       "char* sav_get_string(unsigned int offset, unsigned int codepoints);" },
-    { sav_set_string,       "void sav_set_string(char* string, unsigned int offset, unsigned int codepoints);" },
     { sav_check_value,      "int sav_check_value(enum SAV_CheckValue field, int value);" },
     { party_get_pkx,        "void party_get_pkx(char* data, int slot);" },
     { party_inject_pkx,     "void party_inject_pkx(char* data, enum Generation type, int slot);" },
@@ -36,6 +32,17 @@ struct LibraryFunction UnixFunctions[] =
     { bank_get_pkx,         "char* bank_get_pkx(enum Generation* type, int box, int slot);" },
     { bank_get_size,        "int bank_get_size();" },
     { bank_select,          "void bank_select();" },
+    // general data handling
+    { sav_get_data,         "void sav_get_data(char* dataOut, unsigned int size, int off1, int off2);" },
+    { sav_set_data,         "void sav_set_data(char* data, unsigned int size, int off1, int off2);" },
+    { sav_get_byte,         "char sav_get_byte(int off1, int off2);" },
+    { sav_set_byte,         "void sav_set_byte(char data, int off1, int off2);" },
+    { sav_get_short,        "short sav_get_short(int off1, int off2);" },
+    { sav_set_short,        "void sav_set_short(short data, int off1, int off2);" },
+    { sav_get_int,          "int sav_get_int(int off1, int off2);" },
+    { sav_set_int,          "void sav_set_int(int data, int off1, int off2);" },
+    { sav_get_string,       "char* sav_get_string(int off1, int off2, unsigned int codepoints);" },
+    { sav_set_string,       "void sav_set_string(char* string, int off1, int off2, unsigned int codepoints);" },
     // pkm
     { pkx_encrypt,          "void pkx_decrypt(char* data, enum Generation type, int isParty);" },
     { pkx_decrypt,          "void pkx_encrypt(char* data, enum Generation type, int isParty);" },
@@ -72,6 +79,23 @@ struct LibraryFunction UnixFunctions[] =
     // misc
     { pksm_base64_decode,   "void base64_decode(unsigned char** out, int* outSize, char* data, int size);" },
     { pksm_base64_encode,   "void base64_encode(char** out, int* outSize, unsigned char* data, int size);" },
+    // json
+    { json_new,             "struct JSON* json_new();" },
+    { json_parse,           "void json_parse(struct JSON* out, const char* data);" },
+    { json_delete,          "void json_delete(struct JSON* freed);" },
+    { json_is_valid,        "int json_is_valid(struct JSON* check);" },
+    { json_is_int,          "int json_is_int(struct JSON* check);" },
+    { json_is_bool,         "int json_is_bool(struct JSON* check);" },
+    { json_is_string,       "int json_is_string(struct JSON* check);" },
+    { json_is_array,        "int json_is_array(struct JSON* check);" },
+    { json_is_object,       "int json_is_object(struct JSON* check);" },
+    { json_get_int,         "int json_get_int(struct JSON* get);" },
+    { json_get_bool,        "int json_get_bool(struct JSON* get);" },
+    { json_get_string,      "const char* json_get_string(struct JSON* get);" },
+    { json_array_size,      "int json_array_size(struct JSON* get);" },
+    { json_array_element,   "struct JSON* json_array_element(struct JSON* array, int index);" },
+    { json_object_contains, "int json_object_contains(struct JSON* get);" },
+    { json_object_element,  "struct JSON* json_object_element(struct JSON* object, const char* elemName);" },
     // end
     { NULL,                 NULL }
 };
@@ -80,6 +104,7 @@ void PlatformLibraryInit(Picoc *pc)
 {
     IncludeRegister(pc, "pksm.h", &UnixSetupFunc, &UnixFunctions[0],
     "struct pkx { int species; int form; };"
+    "struct JSON { void* dummy; };"
     "enum Generation { GEN_FOUR, GEN_FIVE, GEN_SIX, GEN_SEVEN, GEN_LGPE, GEN_EIGHT, GEN_THREE };"
     "struct directory { int count; char** files; };"
     "enum PKX_Field {OT_NAME, TID, SID, SHINY, LANGUAGE, MET_LOCATION, MOVE, BALL, LEVEL, GENDER,"
