9176ca2d2072317117d97ef74c24eee845f52927
SNBeast <snburchett@gmail.com>
2022-10-07 13:03:16 -0400

    PkmItemOverlay uses Gen II items for PK1

--- 3ds/source/gui/screen/HexEditScreen.cpp
+++ 3ds/source/gui/screen/HexEditScreen.cpp
@@ -2667,7 +2667,7 @@ void HexEditScreen::drawMeaning() const
                     break;
                 case 0xA:
                     Gui::text(i18n::item2(Configuration::getInstance().language(),
-                                  static_cast<pksm::PK2&>(pkm).heldItem2()),
+                                  static_cast<pksm::PK1&>(pkm).heldItem2()),
                         160, 100, FONT_SIZE_12, COLOR_WHITE, TextPosX::CENTER, TextPosY::TOP);
                     break;
                 case 0xB ... 0xE:
