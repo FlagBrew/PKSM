7baf86ce0032820bb3ee61673f56e7053752bc7b
FM1337 <5448220+FM1337@users.noreply.github.com>
2023-01-01 20:25:27 -0400

    It's building now.
    Good first start, I guess? (Outputs nro but still errors)

---
+++ switch/include/PKSMCORE_CONFIG.h
@@ -0,0 +1,37 @@
+/*
+ *   This file is part of PKSM
+ *   Copyright (C) 2016-2022 Bernardo Giordano, Admiral Fish, piepie62, FM1337
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *   Additional Terms 7.b and 7.c of GPLv3 apply to this file:
+ *       * Requiring preservation of specified reasonable legal notices or
+ *         author attributions in that material or in the Appropriate Legal
+ *         Notices displayed by works containing it.
+ *       * Prohibiting misrepresentation of the origin of that material,
+ *         or requiring that modified versions of such material be marked in
+ *         reasonable ways as different from the original version.
+ */
+
+#ifndef _PKSMCORE_GETLINE_FUNC
+#define _PKSMCORE_GETLINE_FUNC __getline
+#endif
+
+#ifndef _PKSMCORE_LANG_FOLDER
+#define _PKSMCORE_LANG_FOLDER "romfs:/i18n/"
+#endif
+
+#ifndef _PKSMCORE_EXTRA_LANGUAGES
+#define _PKSMCORE_EXTRA_LANGUAGES NL, PT, RU, RO
+#endif
