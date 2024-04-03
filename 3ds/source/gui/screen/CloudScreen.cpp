dd9b5613006fc720dc112916846937dddb230606
FM1337 <5448220+FM1337@users.noreply.github.com>
2024-04-03 18:13:32 -0300

    FIx GPSS Download Codes

--- 3ds/source/gui/screen/CloudScreen.cpp
+++ 3ds/source/gui/screen/CloudScreen.cpp
@@ -928,13 +928,23 @@ void CloudScreen::shareReceive()
                 }
 
                 nlohmann::json retJson = nlohmann::json::parse(retData, nullptr, false);
+                
+                std::string generation = "";
+                if (retJson.contains("generation")) {
+                    if(retJson["generation"].is_string()) {
+                        generation = retJson["generation"].get<std::string>();
+                    } else if(retJson["generation"].is_number()) {
+                        // convert the number to a string
+                        generation = std::to_string(retJson["generation"].get<int>());
+                    }
+                }
+                
 
-                if (retJson.is_object() && retJson.contains("generation") &&
-                    retJson["generation"].is_string() && retJson.contains("pokemon") &&
+                if (retJson.is_object() && generation != "" && retJson.contains("pokemon") &&
                     retJson["pokemon"].is_string())
                 {
                     pksm::Generation gen =
-                        pksm::Generation::fromString(retJson["generation"].get_ref<std::string&>());
+                        pksm::Generation::fromString(generation);
 
                     auto retData = base64_decode(retJson["pokemon"].get_ref<std::string&>());
 
