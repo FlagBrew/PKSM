55dc5111a3ccc61686b62ed3ec10ac681516b043
wowilookgood <omarkhafagi15@gmail.com>
2025-02-01 20:48:47 +0100

    Add better divider

---
+++ switch/include/gui/UIConstants.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <pu/Plutonium>
+
+namespace UIConstants {
+    // Colors
+    static constexpr pu::ui::Color BACKGROUND_BLUE = pu::ui::Color(10, 15, 75, 255);
+    static constexpr pu::ui::Color TEXT_WHITE = pu::ui::Color(255, 255, 255, 255);
+} 
