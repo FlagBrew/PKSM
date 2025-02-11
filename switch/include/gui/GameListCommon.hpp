c7cd25ea1e8ddede1768f26fbaf0b7aab837dc13
wowilookgood <omarkhafagi15@gmail.com>
2025-02-11 21:31:50 +0100

    Add navigation between/to trigger buttons
    Create emulator game list
    
    c
    
    Toggle emulator game list

---
+++ switch/include/gui/GameListCommon.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <pu/Plutonium>
+
+namespace pksm::ui {
+
+struct GameListLayoutConfig {
+    pu::i32 paddingLeft;
+    pu::i32 paddingRight;
+    pu::i32 paddingTop;
+    pu::i32 paddingBottom;
+    pu::i32 sectionTitleSpacing;
+    pu::i32 gameOutlinePadding;
+};
+} 
